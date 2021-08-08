97db5d5d0f9419b60ffa411179986925a95baefc
Jessica Antunes <jessantunes@protonmail.com>
2021-08-08 16:38:02 -0400

    add before action to refactor rspec tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,19 +2,22 @@ require './card'
 require 'spec_helper'
 
 RSpec.describe Card do
+    # before runs before each example
+
+    before do
+        @card = Card.new('Ace', 'Spades')
+    end
     # example group is a group of similar tests
     # rspec meant to be sure readable. it is a method and accepts an argument that is a string
     # describe expected behavior NOT implementation.
     # specify 'has a type' do same as it
 
     it 'has a rank' do
-        card = Card.new('Ace', 'Spades')
-        expect(card.rank).to eq('Ace')
+        expect(@card.rank).to eq('Ace')
     end
 
     it 'has a suit' do
-        card = Card.new('Ace', 'Spades')
-        expect(card.suit).to eq('Spades')
+        expect(@card.suit).to eq('Spades')
     end
 
 end
--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -1,23 +1,23 @@
-class School
-  attr_accessor :name, :students
+# class School
+#   attr_accessor :name, :students
 
-  def initialize(name)
-    @name = name
-    @students = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @students = []
+#   end
 
-end
+# end
 
 
 
-RSpec.describe School do
-  it 'has a name' do
-    school = School.new('Beverly Hills High School')
-    expect(school.name).to eq('Beverly Hills High School')
-  end
+# RSpec.describe School do
+#   it 'has a name' do
+#     school = School.new('Beverly Hills High School')
+#     expect(school.name).to eq('Beverly Hills High School')
+#   end
 
-  it 'should start off with no students' do
-    school = School.new('Notre Dame High School')
-    expect(school.students).to eq([])
-  end
-end
+#   it 'should start off with no students' do
+#     school = School.new('Notre Dame High School')
+#     expect(school.students).to eq([])
+#   end
+# end
